c5f0abd363c2b4597e699c733eb6b0c007ec5d51
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2025-10-24 22:16:45 +0200

    Adding get all users from DB PHP and linking it to Admin table

--- controller/controller.php
+++ controller/controller.php
@@ -1,27 +1,31 @@
 <?php
 require_once '../Config/Database.php';
-require_once '../model/UserBD.php';
+require_once '../model/UserModel.php';
 
 
 class controller {
-    private $UserBd;
+    private $UserModel;
 
     public function __construct() {
         $database = new Database();
         $db = $database->getConnection();
-        $this->UserBd = new UserBd($db);
+        $this->UserModel = new UserModel($db);
     }
 
     public function create_user($username, $pswd1, $pswd2) {
-        return $this->UserBD->create_user($username, $pswd1, $pswd2);
+        return $this->UserModel->create_user($username, $pswd1, $pswd2);
+    }
+
+    public function get_all_users() {
+        return $this->UserModel->get_all_users();
     }
 
     public function modifyProfile($email, $username, $phone, $firstName, $lastName, $profile_code) {
-        return $this->UserBD->buscarPorIsbn($$email, $username, $phone, $firstName, $lastName, $profile_code);
+        return $this->UserModel->buscarPorIsbn($email, $username, $phone, $firstName, $lastName, $profile_code);
     }
 
     public function modifyUser($gender, $card_no, $profile_code){
-        return $this->UserBD->modifyUser($gender, $card_no, $profile_code);
+        return $this->UserModel->modifyUser($gender, $card_no, $profile_code);
     }
 }
 ?>
--- view/assets/js/adminPopup.js
+++ view/assets/js/adminPopup.js
@@ -1,41 +1,35 @@
-// Get the modal
-var modal = document.getElementById("adminTableModal");
+document.addEventListener("DOMContentLoaded", async () => {
+  var modal = document.getElementById("adminTableModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  var adminTable = document.getElementById("adminTable");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-var adminTable = document.getElementById("adminTable");
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+  span.onclick = function () {
     modal.style.display = "none";
-  }
-};
+  };
 
-for (let i = 0; i < 30; i++) {
-  let row = adminTable.insertRow(1);
-  row.className = "adminTableData";
-  let username = row.insertCell(0);
-  let accountNum = row.insertCell(1);
-  let buttons = row.insertCell(2);
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 
-  username.innerHTML = "Profile";
-  accountNum.innerHTML = "xxxx-xxxx-xxxx";
-  buttons.innerHTML = `<div class="center-flex-div">
+  let users = await get_all_users();
+
+  users.forEach((user) => {
+    let row = adminTable.insertRow(1);
+    row.className = "adminTableData";
+    let username = row.insertCell(0);
+    let accountNum = row.insertCell(1);
+    let buttons = row.insertCell(2);
+
+    username.innerHTML = user["USER_NAME"];
+    accountNum.innerHTML = user["CARD_NO"];
+    buttons.innerHTML = `<div class="center-flex-div">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -63,4 +57,13 @@ for (let i = 0; i < 30; i++) {
                   />
                 </svg>
               </div>`;
+  });
+});
+
+async function get_all_users() {
+  const response = await fetch("../../api/GetAllUsers.php");
+  console.log("RESPONSE: ", response);
+  const data = await response.json();
+
+  return data["resultado"];
 }
