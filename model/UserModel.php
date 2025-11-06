0e2026ee5a8802ee3b347159f1a0bb1587385425
Jago128 <jagobaetxegarai@gmail.com>
2025-11-06 13:28:58 +0100

    Error check stuff in UserModel

--- model/UserModel.php
+++ model/UserModel.php
@@ -2,6 +2,13 @@
 require_once 'User.php';
 require_once 'Admin.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+ini_set('log_errors', 1);
+ini_set('error_log', 'php_error.log');
+
 class UserModel
 {
     private $conn;
