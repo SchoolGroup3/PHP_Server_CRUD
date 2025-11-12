b472d76017a76916187ba21af8f48664d86141d8
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2025-11-12 21:05:26 +0100

    Modifications to signup and login javascript

--- view/assets/js/login.js
+++ view/assets/js/login.js
@@ -1,40 +1,42 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", async function (e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", async function (e) {
+      e.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
 
-    let data = await login(username, password);
+      let data = await login(username, password);
 
-    if (data) {
-      if (data["error"]) {
-        alert("El nombre de usuario o la contraseña con incorrectas.");
-      } else {
-        if (data["resultado"]) {
-          let string = JSON.stringify(data["resultado"]);
-          let user = JSON.parse(string);
-          console.log(user);
-          localStorage.setItem("actualProfile", string);
-          window.location.href = "main.html";
+      if (data) {
+        if (data["error"]) {
+          alert("El nombre de usuario o la contraseña con incorrectas.");
+        } else {
+          if (data["resultado"]) {
+            let string = JSON.stringify(data["resultado"]);
+            let user = JSON.parse(string);
+            console.log(user);
+            localStorage.setItem("actualProfile", string);
+            window.location.href = "main.html";
+          }
         }
+      } else {
+        console.log("Error al cargar JSON.");
       }
-    } else {
-      console.log("Error al cargar JSON.");
-    }
-  });
+    });
 
-async function login(username, password) {
-  const response = await fetch(`../../api/Login.php`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, password }),
-  });
+  async function login(username, password) {
+    const response = await fetch(`../../api/Login.php`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
 
-  let data = await response.json();
+    let data = await response.json();
 
-  return data;
-}
+    return data;
+  }
+});
